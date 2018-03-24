991931024abf0beaae581c761b3a2f556052c295
xithiox <xithiox@gmail.com>
2018-03-24 11:02:01 -0500

    Finished basic HTML and CSS, created canvas

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+let bg = [0, 0, 0];
+
+
+/* Main p5.js functions */
+
+function setup() {
+    let c = createCanvas(600, 650);
+    c.parent('game');
+}
+
+function draw() {
+    background(bg);
+}
