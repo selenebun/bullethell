4867045a7da57c9deeff99fd9ab5d2cac07301df
xithiox <xithiox@gmail.com>
2018-03-24 11:13:31 -0500

    Added FPS display

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,20 @@
 let bg = [0, 0, 0];
 
+let avgFPS = 0;
+let numFPS = 0;
+
+let debugMode = false;
+
+
+// Calculate current and average FPS and update sidebar
+function calcFPS() {
+    let fps = frameRate();
+    avgFPS += (fps - avgFPS) / ++numFPS;
+
+    document.getElementById('fps').innerHTML = 'FPS: ' + fps.toFixed(1);
+    document.getElementById('avgfps').innerHTML = 'Avg. FPS: ' + avgFPS.toFixed(1);
+}
+
 
 /* Main p5.js functions */
 
@@ -10,4 +25,17 @@ function setup() {
 
 function draw() {
     background(bg);
+
+    // Update status display
+    calcFPS();
+}
+
+
+/* User input functions */
+
+function keyPressed() {
+    if (key === 'F') {
+        debugMode = !debugMode;
+        document.getElementById('debug').style.display = debugMode ? 'block' : 'none';
+    }
 }
