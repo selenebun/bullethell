d4587edae96f84c06c5aff8b5b6693faee7b7dfa
xithiox <xithiox@gmail.com>
2018-03-30 15:53:33 -0500

    Added some basic models

---
+++ scripts/template/models.js
@@ -0,0 +1,47 @@
+const MODEL = {};
+MODEL.bullet = {};
+MODEL.ship = {};
+
+
+// Bullet models
+
+MODEL.bullet.basic = function() {
+    fill(this.color);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, this.r, this.r);
+};
+
+
+// Ship models
+
+MODEL.ship.basic = function() {
+    push();
+    translate(this.pos.x, this.pos.y);
+
+    // Exhaust
+    fill('#E74C3C');
+    noStroke();
+    triangle(-3, 14, 3, 14, random(-2, 2), random(20, 24));
+
+    // Thruster
+    fill('#7C8A99');
+    stroke(0);
+    rectMode(RADIUS);
+    rect(-0.5, 3.5, 6, 10);
+
+    // Rear fins
+    fill('#ACBAC9');
+    triangle(3, 2, 3, 10, 22, 16);
+    triangle(-3, 2, -3, 10, -22, 16);
+
+    // Wings
+    fill('#BDC3C7');
+    triangle(1, 3, 1, -24, 12, 6);
+    triangle(-1, 3, -1, -24, -12, 6);
+
+    // Canopy
+    fill(this.color);
+    ellipse(0, 1, 6, 8);
+
+    pop();
+};
